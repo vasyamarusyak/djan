1b4f95f35d582a7b09edecb7280e6ed27406f1c9
Vasya <vasya_marusyak@i.ua>
2017-01-29 20:43:59 +0200

    createproject

---
+++ bin/django-admin.py
@@ -0,0 +1,4 @@
+#!/home/west/djangoenv/bin/python3.5
+# EASY-INSTALL-SCRIPT: 'Django==1.10.5','django-admin.py'
+__requires__ = 'Django==1.10.5'
+__import__('pkg_resources').run_script('Django==1.10.5', 'django-admin.py')
